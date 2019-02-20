0ea6e2ca10ad1cf5d108d7f22614b86eeb89e270
Darwin Shameran <dxsh@riseup.net>
2019-02-20 08:30:49 +0100

    Fix private member data name

--- src/Memory.h
+++ src/Memory.h
@@ -8,7 +8,7 @@ class Memory {
  private:
  	 constexpr std::size_t MEM_SIZE = 0x1000;	/* Memory map size. */
 	 constexpr long MEM_START_AT = 0x200;		/* Start of ROM and work RAM. */
-	 std::array<uint8_t, MEM_SIZE> memory = {};
+	 std::array<uint8_t, MEM_SIZE> m_memory = {};
 
  public:
 	 uint8_t& operator[](std::size_t index);
