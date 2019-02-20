d4f9690f8d4ac4feeb9f6b7fe9cd1988177f1350
Darwin Shameran <dxsh@riseup.net>
2019-02-20 08:53:32 +0100

    Move constexpr vars to global namespace; make index param in subscript overload const

--- src/Memory.h
+++ src/Memory.h
@@ -4,14 +4,16 @@
 #include <vector>
 #include <array>
 
+/* TODO: implement these constexpr vars using the namespace identifier? */
+constexpr std::size_t MEM_SIZE = 0x1000;	/* Memory map size. */
+constexpr long MEM_START_AT = 0x200;		/* Start of ROM and work RAM. */
+
 class Memory {
  private:
- 	 constexpr std::size_t MEM_SIZE = 0x1000;	/* Memory map size. */
-	 constexpr long MEM_START_AT = 0x200;		/* Start of ROM and work RAM. */
 	 std::array<uint8_t, MEM_SIZE> m_memory = {};
 
  public:
-	 uint8_t& operator[](std::size_t index);
+	 uint8_t& operator[](const std::size_t index);
 	 void rom(std::vector<int8_t>&& buf);
 };	
 #endif
